ce8150dba940260933269579f74243bc55279115
lengochuyen25 <huyenln.hvnh@gmail.com>
2019-10-25 17:17:16 +0700

    you can do it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NameCardComponent } from './name-card/name-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NameCardComponent,
+
   ],
   imports: [
     BrowserModule,
